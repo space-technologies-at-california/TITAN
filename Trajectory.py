584b1eebfdd341e977a427dbf225176621cd158c
Aditi Anand <aditibanand@berkeley.edu>
2023-04-06 22:29:25 -0700

    added methods

---
+++ Trajectory.py
@@ -0,0 +1,20 @@
+class Trajectory:
+    
+    #dictionary variables
+
+    def __init__(self):
+        self.numTimeStamps = 0
+        self.state = {}
+
+    def getData():
+        return 0
+
+    def updateState():
+        numTimeStamps+=1
+        state.update(numTimeStamps, getData())
+
+    def obstacleDetector():
+        return 0
+    
+    def calculateTrajectory():
+        return 0
